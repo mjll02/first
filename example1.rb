0920b03fc6b78b98e2cb65690f02ae516e37afd7
Manuel Larreategui <m.larreategui@gmail.com>
2016-09-04 19:45:22 +0000

    first commit

---
+++ example1.rb
@@ -0,0 +1,11 @@
+def something(number)
+    if number > 0
+        puts "positive"
+    else 
+        puts "negative"
+    end 
+end 
+
+something(-1)
+
+    
---
+++ example2.rb
@@ -0,0 +1,11 @@
+def something(number)
+    if number > 0
+        puts "positive"
+    elsif number == 0
+        puts "0"
+    else
+        puts "negative"
+    end    
+end
+
+something(-1)
